858b45f34d8c34e0f8863d5fa73b6da9c9a9ebba
AndreyProshkin <andrey_pro@inbox.ru>
2021-05-17 23:06:23 +0300

    Initial commit

---
+++ src/main/java/com/game/entity/Player.java
@@ -0,0 +1,16 @@
+package com.game.entity;
+
+import java.util.Date;
+
+public class Player {
+    private long id;
+    private String name;
+    private String title;
+    private Race race;
+    private Profession profession;
+    private Integer experience;
+    private Integer level;
+    private Integer untilNextLevel;
+    private Date birthday;
+    private boolean banned;
+}
